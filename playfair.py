90e3dae3c26c1b1d96eb838245bf74a074b8c44f
mamboo108 <147160053+mamboo108@users.noreply.github.com>
2026-03-10 19:02:36 +0530

    Update playfair.py

--- playfair.py
+++ playfair.py
@@ -13,11 +13,37 @@ def pos(x):
         for j in range(5):
             if m[i][j]==x: return i,j
 
+
 t=input("Text: ").upper().replace("J","I")
+
+cipher=""
+
+# Encryption
 for i in range(0,len(t),2):
     a=t[i]; b=t[i+1] if i+1<len(t) else "X"
     r1,c1=pos(a); r2,c2=pos(b)
 
-    if r1==r2: print(m[r1][(c1+1)%5]+m[r2][(c2+1)%5],end="")
-    elif c1==c2: print(m[(r1+1)%5][c1]+m[(r2+1)%5][c2],end="")
-    else: print(m[r1][c2]+m[r2][c1],end="")
+    if r1==r2:
+        cipher+=m[r1][(c1+1)%5]+m[r2][(c2+1)%5]
+    elif c1==c2:
+        cipher+=m[(r1+1)%5][c1]+m[(r2+1)%5][c2]
+    else:
+        cipher+=m[r1][c2]+m[r2][c1]
+
+print("Encrypted:",cipher)
+
+plain=""
+
+# Decryption
+for i in range(0,len(cipher),2):
+    a=cipher[i]; b=cipher[i+1]
+    r1,c1=pos(a); r2,c2=pos(b)
+
+    if r1==r2:
+        plain+=m[r1][(c1-1)%5]+m[r2][(c2-1)%5]
+    elif c1==c2:
+        plain+=m[(r1-1)%5][c1]+m[(r2-1)%5][c2]
+    else:
+        plain+=m[r1][c2]+m[r2][c1]
+
+print("Decrypted:",plain)
