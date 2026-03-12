c4af8ace21673f1484e0b7f4d5e9362f5493288d
mamboo108 <147160053+mamboo108@users.noreply.github.com>
2026-03-12 17:24:55 +0530

    Update playfair.py

--- playfair.py
+++ playfair.py
@@ -31,19 +31,3 @@ for i in range(0,len(t),2):
         cipher+=m[r1][c2]+m[r2][c1]
 
 print("Encrypted:",cipher)
-
-plain=""
-
-# Decryption
-for i in range(0,len(cipher),2):
-    a=cipher[i]; b=cipher[i+1]
-    r1,c1=pos(a); r2,c2=pos(b)
-
-    if r1==r2:
-        plain+=m[r1][(c1-1)%5]+m[r2][(c2-1)%5]
-    elif c1==c2:
-        plain+=m[(r1-1)%5][c1]+m[(r2-1)%5][c2]
-    else:
-        plain+=m[r1][c2]+m[r2][c1]
-
-print("Decrypted:",plain)
